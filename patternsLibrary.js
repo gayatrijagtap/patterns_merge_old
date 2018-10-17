42e9ef26736f5aecd8f7bb99c15b779d0584835a
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-17 14:25:02 +0530

    code refactored.

--- patternsLibrary.js
+++ patternsLibrary.js
@@ -1,20 +1,22 @@
-const repeatChar = function(start,end,character) {
+const insertChar = function(width,startChar,midChar,endChar) {
   let repeatedCharArray = [];
-  for(let i=start; i<end; i++) {
-    repeatedCharArray[i]=character;
+  repeatedCharArray[0] = [startChar];
+  repeatedCharArray[width] = [endChar]
+  for(let i=1; i<width-1; i++) {
+    repeatedCharArray[i]=midChar;
   }
   return repeatedCharArray;
 }
 
-exports.repeatChar = repeatChar;
+exports.insertChar = insertChar;
 
 const joinCharacters = function(repeatedCharArray) {
   let joinedChars = repeatedCharArray.join('');
   return joinedChars;
 }
 
-const createLine = function(start,end,character) {
-  let repeatedCharArray = repeatChar(start,end,character);
+const createLine = function(width,startChar,midChar,endChar) {
+  let repeatedCharArray = insertChar(width,startChar,midChar,endChar);
   let line = joinCharacters(repeatedCharArray);
   return line;
 }
@@ -24,9 +26,19 @@ exports.createLine = createLine;
 const createFilledRect = function(width,height) {
   let filledRectArray = [];
   for(let i=0; i<height; i++) {
-    filledRectArray[i] = createLine(0,width,'*');
+    filledRectArray[i] = createLine(width,'*','*','*');
   }
   return filledRectArray;
 }
 
+const printFilledRectangle = function(width,height) {
+  let filledRectangleArray = createFilledRect(width,height);
+  for(let i=0; i<filledRectangleArray.length; i++) {
+    console.log(filledRectangleArray[i]);
+  }
+  return;
+}
+
 exports.createFilledRect = createFilledRect;
+
+printFilledRectangle(5,5);
