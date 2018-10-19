359846a92a60f261847b1778d1e1122195f71eb5
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-19 13:11:46 +0530

    Refactored stars in test into variables

--- tests/testCreateFilledRect.js
+++ tests/testCreateFilledRect.js
@@ -1,6 +1,11 @@
 const assert = require('assert');
 const createFilledRect = require('../patternsLibrary.js').createFilledRect;
 
-assert.deepEqual(createFilledRect(5,5),['*****','*****','*****','*****','*****']);
+const _5Stars = '*****', _20Stars = '********************';
+
+assert.deepEqual(createFilledRect(5,5),
+  [_5Stars,_5Stars,_5Stars,_5Stars,_5Stars]);
 assert.deepEqual(createFilledRect(2,2),['**','**']);
 assert.deepEqual(createFilledRect(1,1),['*']);
+assert.deepEqual(createFilledRect(20,7),
+  [_20Stars,_20Stars,_20Stars,_20Stars,_20Stars,_20Stars,_20Stars]); 
